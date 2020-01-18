1d563556c046d28e62d78c87c0197c65ee01fb56
paramest <paramest.wsw@gmai.com>
2020-01-18 18:02:05 +0700

    update code

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,53 +1,227 @@
 <template>
 	<div class="profile-container">
-		<nav class="nav-bar">
+
+		<nav style="padding: 20px; border-bottom: 3px solid #ccc; margin-bottom: 60px;">
 			<div class="container">
-				<h2>
-					<i class="nes-icon coin"></i>
+				<h2 style="margin-bottom: 0;">
+					<i class="snes-jp-logo"></i>
 					Paramas
 				</h2>
 			</div>
 		</nav>
 
-		<div class="container mr-b-25">
+		<div class="container" style="margin-bottom: 50px;">
 			<div class="row">
 				<div class="col">
 					<i class="nes-bcrikko"></i>
 				</div>
 				<div class="col-sm-9 col-xs-12">
 					<div class="nes-balloon from-left">
-						<p>
-							I'm Paramas. This is everything about me.
-							<i class="nes-icon heart is-small"></i>
-						</p>
+						<p>I'm Paramas. This is everything about me.</p>
 					</div>
 				</div>
 			</div>
 		</div>
+
 		<div class="container">
 			<div class="row">
+				<!-- left -->
 				<div class="col-md-4 col-sm-12 col-xs-12">
-					<div class="nes-container with-title mr-t-25">
+					<div class="nes-container with-title">
 						<h3 class="title">Things I do</h3>
-						<div class="item">
-							<ul class="nes-list is-disc">
-								<li>Web Developer</li>
-								<li>Dev OPS</li>
-							</ul>
-						</div>
-					</div>
-					<div class="nes-container is-dark with-title mr-t-25">
-						<h3 class="title">My Socials</h3>
-						<div class="item">
-							<div class="icon-list">
-								<a href="#"><i class="nes-icon facebook is-medium"></i></a>
-								<a href="#"><i class="nes-icon instagram is-medium"></i></a>
-								<a href="#"><i class="nes-icon github is-medium"></i></a>
+						<div>
+							<div class="lists">
+								<ul class="nes-list is-circle" style="margin-bottom: 0;">
+									<li>Web Develop</li>
+									<li>Dev OPS</li>
+								</ul>
 							</div>
 						</div>
 					</div>
+					<div class="nes-container with-title is-dark" style="margin-top: 50px; margin-bottom: 50px;">
+						<div class="title">Social Media</div>
+						<div>
+							<a href="https://www.instagram.com/paramest.wsw">
+								<i class="nes-icon instagram is-medium"></i>
+							</a>
+							<a href="https://twitter.com/WhoIsPor">
+								<i class="nes-icon twitter is-medium"></i>
+							</a>
+							<a href="https://web.facebook.com/paramas.wae">
+								<i class="nes-icon facebook is-medium"></i>
+							</a>
+							<a href="https://github.com/deVitarXD">
+								<i class="nes-icon github is-medium"></i>
+							</a>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-4 col-sm-12 col-xs-12">Right</div>
+				<!-- right      -->
+				<div class="col-md-8 col-sm-12 col-xs-12">
+					<div class="nes-container with-title">
+						<h3 class="title">Frontend Knowledge</h3>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">HTML[5]</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="95" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Javascript</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="75" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Jquery</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="75" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">CSS[3]</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-warning" value="25" max="100">
+								</progress>
+							</div>
+						</div>
+					</div>
+
+					<div class="nes-container with-title is-dark" style="margin-top: 40px;">
+						<h3 class="title">ฺBackend Knowledge</h3>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">PHP</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="90" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Docker</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="80" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Node. js</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="55" max="100">
+								</progress>
+							</div>
+						</div>
+					</div>
+
+					<div class="nes-container with-title" style="margin-top: 40px; margin-bottom: 40px">
+						<h3 class="title">Other Knowledge</h3>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Laravel</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="85" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Vue</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="85" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">Git</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="70" max="100">
+								</progress>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4 col-xs-12">React</div>
+							<div class="col-sm-8 col-xs-12">
+								<progress class="nes-progress is-success" value="40" max="100">
+								</progress>
+							</div>
+						</div>
+					</div>
+
+					<h3 class="topic-title">
+						<i class="nes-icon star"></i> Experience
+					</h3>
+					<div class="nes-container is-dark">
+						<div class="row">
+							<div class="col-xs-2">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>PRINCE OF SONGKLA UNIVERSITY, PHUKET CAMPUS</h3>
+								<p>Technology and Environment (Information Technology) Majors</p>
+								<p>GPA: 2.97/4.00</p>
+								<p>2012 - 2015</p>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 30px;">
+							<div class="col-xs-2 icon-list">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>CDG Systems Ltd</h3>
+								<p>JAVA Programer(Cooperative education program)</p>
+								<p>2015</p>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 30px;">
+							<div class="col-xs-2">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>IMT ASE Co., LTD.</h3>
+								<p>Web Developer (Full Stack)</p>
+								<p>2016 ( 16 Jun - 28 Sep )</p>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 30px;">
+							<div class="col-xs-2">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>IMT ASE Co., LTD.</h3>
+								<p>Web Developer (Full Stack)</p>
+								<p>2016 ( 16 Jun - 28 Sep )</p>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 30px;">
+							<div class="col-xs-2">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>Jobbkk.com</h3>
+								<p>Web Programer (PHP, Codeigniter2)</p>
+								<p>1 Oct 2016 - 1 March  2018</p>
+							</div>
+						</div>
+						<div class="row" style="margin-top: 30px;">
+							<div class="col-xs-2">
+								<i class="nes-pokeball is-large"></i>
+							</div>
+							<div class="col-xs-10">
+								<h3>Nilecon (Thailand) Co., Ltd.</h3>
+								<p>Web Develop (PHP, Nodejs, Reactjs, Docker, Angular2)</p>
+								<p>2018 ( 5 March ) - PRESENT</p>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="container" style="margin-top: 50px; font-size: 90%;">
+			<div class="row">
+				<div class="col-xs-12" style="text-align: center; color: #a9f9a9; background: #000;">
+					Using NES.css 2019
+				</div>
 			</div>
 		</div>
 	</div>
@@ -59,6 +233,14 @@
 	};
 </script>
 <style>
+	.nes-icon {
+		margin-right: 25px;
+	}
+
+	.nes-icon.is-medium {
+		margin-right: 45px;
+	}
+
 	.nav-bar {
 		padding: 20px;
 		border-bottom: 3px solid #ccc;
